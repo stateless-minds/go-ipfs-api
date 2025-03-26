35c2fb4e2f04539972344aec9d1191dcc3281bb3
mar1n3r0 <m.kirkov@gmail.com>
2025-03-26 17:40:24 +0200

    add check keys

--- shell.go
+++ shell.go
@@ -776,6 +776,21 @@ func (s *Shell) OrbitDocsDelete(dbName, key string) error {
 	return nil
 }
 
+func (s *Shell) OrbitCheckKyes(descriptor string) error {
+	// connect
+	encoder, _ := mbase.EncoderByName("base64url")
+	resp, err := s.Request("orbit/checkkeys", encoder.Encode([]byte(descriptor))).Send(context.Background())
+	if err != nil {
+		return err
+	}
+	if resp.Error != nil {
+		resp.Close()
+		return resp.Error
+	}
+
+	return nil
+}
+
 func (s *Shell) RunInflationIndexer() error {
 	// connect
 	resp, err := s.Request("orbit/runindexer").Send(context.Background())
